0c9c37d1dc4752aad6d8c45d5004bdbe1294eb00
Priyanshu Kumar <157512127+Priyanshu-kumar5511@users.noreply.github.com>
2026-08-04 01:42:48 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+
+        int n=nums.size();
+        nums.resize(2 * n);
+        int j=0;
+        for(int i=n;i<nums.size();i++)
+
+        {
+            if(j != n)
+            {
+                nums[i]= nums[j];
+                j++;
+            }
+        }
+        return nums;
+    }
+};
